6edf8d328fd0640ed999bb28ee91b201586d343b
Jyry Niemitalo <jyniemit@student.hive.fi>
2025-04-17 18:36:39 +0300

    feat(header): added unistd for write, and fixed endif location to bottom

--- include/printf.h
+++ include/printf.h
@@ -6,18 +6,19 @@
 /*   By: jyniemit <jyniemit@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 11:48:02 by jyniemit          #+#    #+#             */
-/*   Updated: 2025/04/17 11:56:13 by jyniemit         ###   ########.fr       */
+/*   Updated: 2025/04/17 17:28:26 by jyniemit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef    PRINTF_H
 # define    PRINTF_H
-# endif
 
 //for INT_MIN & INT_MAX
 #include <limits.h>
 //args
 #include <stdarg.h>
-
+//write
+#include <unistd.h>
 //prototypes
 int	ft_printf(char *format, ...);
+# endif
