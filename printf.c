7ed2efb9bf7034cc6d3888127803b9e589e680a2
Jyry Niemitalo <jyniemit@student.hive.fi>
2025-04-17 18:37:31 +0300

    feat(printf): handle string

--- printf.c
+++ printf.c
@@ -3,6 +3,20 @@
 //debug
 #include <stdio.h>
 
+int	handle_s(va_list args)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	str = va_arg(args, char *);
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	return (i);
+}
 int	main(void)
 {
 	printf("testing if I can still code anything. :D");
