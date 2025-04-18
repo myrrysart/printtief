66c55321af80f7730654f7d99f8ce7ea3c4a64b7
Jyry Niemitalo <jyniemit@student.hive.fi>
2025-04-18 17:36:18 +0300

    feat(string): handle strings

---
+++ src/handle_s.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_s.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jyniemit <jyniemit@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/18 14:26:29 by jyniemit          #+#    #+#             */
+/*   Updated: 2025/04/18 16:13:32 by jyniemit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "printf.h"
+
+int	handle_s(va_list args)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	str = va_arg(args, char *);
+	if (!str)
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	return (i);
+}
