89d797faedbeb08750f7d15a4ac1c54b28d2a7e0
Jyry Niemitalo <jyniemit@student.hive.fi>
2025-04-18 17:36:41 +0300

    feat(hex): handle lowercase hex

---
+++ src/handle_x.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_x.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jyniemit <jyniemit@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/18 14:24:27 by jyniemit          #+#    #+#             */
+/*   Updated: 2025/04/18 16:16:14 by jyniemit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "printf.h"
+
+int	handle_x(va_list args)
+{
+	char	str[16];
+	int		i;
+	int		num;
+	int		count;
+
+	i = 0;
+	count = 0;
+	num = va_arg(args, int);
+	if (num == 0)
+	{
+		write(1, "(nil)", 5);
+		return (5);
+	}
+	while (num > 0)
+	{
+		str[i] = HEXLOW[num % 16];
+		num /= 16;
+		i++;
+	}
+	while (i > 0)
+	{
+		i--;
+		write(1, &str[i], 1);
+		count++;
+	}
+	return (count);
+}
