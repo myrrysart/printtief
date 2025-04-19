d6190da0e061867b259cdc738cf1ae19582a5d6e
Jyry Niemitalo <jyniemit@student.hive.fi>
2025-04-19 13:10:57 +0300

    refactor(d):added static helper

--- src/handle_d.c
+++ src/handle_d.c
@@ -6,22 +6,20 @@
 /*   By: jyniemit <jyniemit@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 14:25:58 by jyniemit          #+#    #+#             */
-/*   Updated: 2025/04/18 15:52:35 by jyniemit         ###   ########.fr       */
+/*   Updated: 2025/04/19 12:45:25 by jyniemit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "printf.h"
 
+static int	print_nbr(int num);
 
 int	handle_d(va_list args)
 {
-	char	str[11];
-	int		i;
 	int		num;
 	int		ret;
 
-	i = 0;
 	ret = 0;
-	num	= va_arg(args, int);
+	num = va_arg(args, int);
 	if (num == 0)
 	{
 		write(1, "0", 1);
@@ -32,12 +30,24 @@ int	handle_d(va_list args)
 		write(1, "-2147483648", 11);
 		return (11);
 	}
-	while (num < 0)
+	if (num < 0)
 	{
 		write(1, "-", 1);
 		ret++;
 		num *= -1;
 	}
+	ret += print_nbr(num);
+	return (ret);
+}
+
+static int	print_nbr(int num)
+{
+	char	str[11];
+	int		i;
+	int		ret;
+
+	i = 0;
+	ret = 0;
 	while (num > 0)
 	{
 		str[i] = num % 10 + 48;
@@ -45,10 +55,7 @@ int	handle_d(va_list args)
 		i++;
 		ret++;
 	}
-	while (i > 0)
-	{
-		i--;
+	while (i-- > 0)
 		write(1, &str[i], 1);
-	}
 	return (ret);
 }
